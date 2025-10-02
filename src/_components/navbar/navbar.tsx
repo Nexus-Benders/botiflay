e49472a1ab76b3bfd8bf0addb2aa4f7586561cdc
naffiziqbal <f.nafiziqbal@gmail.com>
2025-10-02 10:51:06 +0600

    Enhance Navbar component by integrating dynamic navigation links from constants. Updated layout for improved styling and responsiveness.

--- src/_components/navbar/navbar.tsx
+++ src/_components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Container from "../layout/container";
 import Link from "next/link";
+import { NAV_LINKS } from "@/constants/nav-links";
 
 export default function Navbar() {
   return (
@@ -13,21 +14,20 @@ export default function Navbar() {
 
 const DesktopNavbar = () => {
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-row justify-between items-center py-2">
       <figure>
         <Image src="/logo.png" alt="Botiflay logo" width={100} height={100} />
       </figure>
-      <div className="flex-1 flex justify-center items-center">
-        <ul className="flex flex-row justify-between">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/">About</Link>
-          </li>
-          <li>
-            <Link href="/">Services</Link>
-          </li>
+      <div className="flex-1 flex justify-center items-center ">
+        <ul className="flex flex-row gap-8  justify-between bg-[#15151514] w-fit px-8 py-4 rounded-full">
+          {NAV_LINKS.map((link: { href: string; label: string }) => (
+            <li key={link.href} className="group">
+              <Link href={link.href} className="relative">
+                {link.label}
+              </Link>
+              <div className="origin-left scale-x-0 group-hover:scale-x-100 transition-transform duration-300 border-b-2 border-[#BDED5B]" />
+            </li>
+          ))}
         </ul>
       </div>
       <button>Start Your Project</button>
---
+++ src/constants/nav-links.ts
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: "Work",
+    href: "/",
+  },
+  {
+    label: "Service",
+    href: "/",
+  },
+  {
+    label: "Process",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+];
