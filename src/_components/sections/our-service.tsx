bad62f09e8ef0a74ff889d9d2e8a03a0203355ff
naffiziqbal <f.nafiziqbal@gmail.com>
2025-10-03 21:25:37 +0600

    Refactor OurService section layout by adjusting class names for improved styling consistency and enhancing the figure element's size for better visual presentation.

--- src/_components/sections/our-service.tsx
+++ src/_components/sections/our-service.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 export default function OurService() {
   return (
-    <div className="flex justify-center items-center flex-col gap-14 h-dvh  text-center py-12 text-white">
+    <div className="flex justify-center items-center flex-col gap-14  h-dvh   text-center py-12 text-white">
       <SectionHeader hasBlackBg headline={"Our Service"} />
-      <figure className="relative size-full pointer-events-none">
+      <figure className="relative  pointer-events-none size-10/12">
         <Image src={"/assets/service.png"} alt="service-image" fill />
       </figure>
     </div>
