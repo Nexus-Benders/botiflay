eaebacc3f07347898a216d7b258e6d6b55bd0584
naffiziqbal <f.nafiziqbal@gmail.com>
2025-10-15 20:56:50 +0600

    Refactor SectionHeader component to enhance text presentation and layout consistency. Update description prop to accept React nodes and improve text wrapping for better visual alignment across devices.

--- src/_components/headers/section-header.tsx
+++ src/_components/headers/section-header.tsx
@@ -1,14 +1,18 @@
+import React from "react";
+
 export default function SectionHeader({
   headline,
-  description = "Creative solutions, seamless collaboration, and impactful results.",
+  description = (
+    <p>Creative solutions, seamless collaboration, and impactful results.</p>
+  ),
   hasBlackBg = false,
 }: {
   headline: React.ReactNode;
-  description?: string;
+  description?: React.ReactNode;
   hasBlackBg?: boolean;
 }) {
   return (
-    <div className="font-space-grotesk flex flex-col gap-2 sm:gap-[8px] px-4">
+    <div className="font-space-grotesk flex flex-col gap-2 sm:gap-[8px] px-4 text-wrap justify-center items-center">
       <div
         className={`font-bold text-3xl md:text-4xl lg:text-5xl xl:text-[56px] leading-[1.2] sm:leading-[1.25] uppercase ${
           hasBlackBg ? "text-white" : "text-[#262626]"
@@ -18,7 +22,7 @@ export default function SectionHeader({
       </div>
 
       <div
-        className={`text-sm sm:text-base md:text-lg lg:text-[18px] leading-[1.6] sm:leading-[1.7] md:leading-[1.8] ${
+        className={`text-sm sm:text-base md:text-lg lg:text-[18px] leading-[1.6] sm:leading-[1.7] md:leading-[1.8] text-wrap ${
           hasBlackBg ? "text-white" : "text-[#434343]"
         }`}
       >
