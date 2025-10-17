e7177b18c3a8f9e1d8b1463a01207382439e8357
naffiziqbal <f.nafiziqbal@gmail.com>
2025-10-17 11:58:38 +0600

    Update OurService component to adjust CSS class for minimum height and modify desktop image aspect ratio, enhancing layout consistency across devices.

--- src/_components/sections/our-service.tsx
+++ src/_components/sections/our-service.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function OurService() {
   return (
-    <div className="flex justify-center items-center flex-col gap-8 sm:gap-10 md:gap-12 lg:gap-14 min-h-[60vh] sm:min-h-[70vh] md:min-h-[80vh] lg:h-dvh text-center py-8 sm:py-10 md:py-12 text-white">
+    <div className="flex justify-center items-center flex-col gap-8 sm:gap-10 md:gap-12 lg:gap-14 min-h-[60vh] sm:min-h-[70vh] md:min-h-[80vh] lg:h-min-dvh text-center py-8 sm:py-10 md:py-12 text-white">
       <SectionHeader hasBlackBg headline={"Our Service"} />
       {/* Mobile Service Image */}
       <figure className="relative pointer-events-none w-full max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[60%] aspect-[9/16] sm:aspect-[4/3] md:aspect-[3/2] lg:aspect-[4/3] lg:hidden">
@@ -17,7 +17,7 @@ export default function OurService() {
       </figure>
 
       {/* Desktop Service Image */}
-      <figure className="relative pointer-events-none w-full max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[60%] aspect-[4/3] hidden lg:block">
+      <figure className="relative pointer-events-none w-full max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[90%]  aspect-[4/3] hidden lg:block">
         <Image
           src={"/assets/service.png"}
           alt="service-image-desktop"
